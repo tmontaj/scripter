19e9000a1e1d0787375c06834eb0fd3eff68c41f
Ubuntu <n.magdy@nu.edu.eg>
2021-02-19 23:20:20 +0000

    Closes #9

---
+++ wav2let/ctc_decoder.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+
+# inputs of shape (batch, time_stips, features)
+def ctc_decoder(inputs, sequence_length=None, beam_width=10, top_paths=1):
+
+  #if not padded
+  if not sequence_length:
+    sequence_length = [inputs.shape[1]]
+    ishape = inputs.shape
+    inputs = tf.reshape(inputs, (ishape[1], ishape[0], ishape[2]))
+  else:
+    inputs = tf.stack(inputs.numpy().tolist(), axis=1)
+
+  print(inputs)
+  decode = tf.nn.ctc_beam_search_decoder(inputs=inputs, sequence_length=sequence_length, beam_width=beam_width, top_paths=top_paths)
+
+  return decode
